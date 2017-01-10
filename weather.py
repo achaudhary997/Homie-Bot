7e176680595850f048db20bef7d2269ba0dcb6a0
YashitM <yashitmaheshwary@gmail.com>
2017-01-10 21:30:54 +0530

    Added script for weather and google images

---
+++ weather.py
@@ -0,0 +1,10 @@
+import pyowm
+
+owm = pyowm.OWM('8e47cb932d1448c4049c3506aca77f87')
+place = "Moscow"
+observation = owm.weather_at_place(place)
+w = observation.get_weather()
+complete_temp = w.get_temperature('celsius') 
+for i in complete_temp:
+	if(i=="temp"):
+		print str(complete_temp[i]) + chr(248) + "C"
