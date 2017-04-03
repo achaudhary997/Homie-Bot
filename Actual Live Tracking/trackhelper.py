c045ea4c683a1c89091420ff7cc6526427ecf691
YashitM <yashitmaheshwary@gmail.com>
2017-04-03 23:32:53 +0530

    Improved Speech Recog

--- Actual Live Tracking/trackhelper.py
+++ Actual Live Tracking/trackhelper.py
@@ -11,22 +11,34 @@ with sr.Microphone() as source:
 	audio=r.listen(source)
 
 inputString = r.recognize_google(audio)
+inputString = inputString.lower()
 listObj = {'bottle':11, 'book':11, 'cube':8, 'mug':5, 'perfume':6, 'mouse':6}
 flag = 0
 objectToFind = ""
-for i in listObj:
-	if i in inputString:
-		objectToFind = i
-		objectThreshold = listObj[i]
-		break
+if "botal" in inputString:
+	objectToFind = "bottle"
+elif "buk" in inputString:
+	objectToFind = "book"
+elif "cute" in inputString:
+	objectToFind = "cube"
+elif "mum" in inputString or "cup" in inputString or "class" in inputString:
+	objectToFind = "mug" 
+elif "deodorant" in inputString:
+	objectToFind = "perfume"
+elif "house" in inputString or "maus" in inputString:
+	objectToFind = "mouse"
+else:
+	for i in listObj:
+		if i in inputString:
+			objectToFind = i
+			objectThreshold = listObj[i]
+			break
 if objectToFind != "":
 	img_files=glob.glob(objectToFind+'/*')
 	flag = 0
-	flag1 = 0
 	count = 0
 	while True: 
 		for i in img_files:
-			start_time = time.time()
 			print "[+] Trying image " + i
 			ret = track.trackObject(i,objectThreshold)
 			if ret == True:
