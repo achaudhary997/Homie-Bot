e623819c5a329d1d7f83b8251ca3d5a672d33281
YashitM <yashitmaheshwary@gmail.com>
2017-04-03 23:15:27 +0530

    Improved Live tracking

--- Actual Live Tracking/trackhelper.py
+++ Actual Live Tracking/trackhelper.py
@@ -10,7 +10,6 @@ with sr.Microphone() as source:
 	print("say something")
 	audio=r.listen(source)
 
-
 inputString = r.recognize_google(audio)
 listObj = {'bottle':11, 'book':11, 'cube':8, 'mug':5, 'perfume':6, 'mouse':6}
 flag = 0
@@ -20,7 +19,6 @@ for i in listObj:
 		objectToFind = i
 		objectThreshold = listObj[i]
 		break
-print (objectThreshold)
 if objectToFind != "":
 	img_files=glob.glob(objectToFind+'/*')
 	flag = 0
@@ -29,27 +27,32 @@ if objectToFind != "":
 	while True: 
 		for i in img_files:
 			start_time = time.time()
-			print "[*] Trying image " + i
+			print "[+] Trying image " + i
 			ret = track.trackObject(i,objectThreshold)
 			if ret == True:
 				print "Found " + objectToFind
 				try:
 					os.system('espeak "' + "found " + objectToFind + '"') 
 				except sr.UnknownValueError:
-					print("error")
+					print("[-] Error")
 				except sr.RequestError as e:
-					print"error"
+					print "[-] Error"
 				flag = 1
 				break
 			else:
 				try:
 					os.system('espeak "' + "lol hogaya" + '"') 
-				except sr.UnknownValueError:
-					print("error")
+				xcept sr.UnknownValueError:
+					print("[-] Error")
 				except sr.RequestError as e:
-					print"error"
+					print "[-] Error"
 		if(flag == 1):
 			break
 
 else:
-	print "Object not in database."
+	try:
+		os.system('espeak "' + "Object not in Database" + '"') 
+	except sr.UnknownValueError:
+		print("[-] Error")
+	except sr.RequestError as e:
+		print "[-] Error"
