73672af57510c0bb5cafc4a4bc5e30307dfc8513
dvatsav <deepak.srivatsav@gmail.com>
2017-04-04 02:08:48 -0700

    updated homie.py

--- homie.py
+++ homie.py
@@ -4,6 +4,7 @@ import pyowm
 from lxml import html
 import requests
 import time
+from datetime import datetime
 r=sr.Recognizer()
 
 def weather():
@@ -15,16 +16,16 @@ def weather():
 
 			
 		try:
-			os.system('espeak "' + "Finding the temperature in " + r.recognize_google(audio) + '"') 
+			os.system('./speech.sh ' + "Finding the temperature in " + r.recognize_google(audio)) 
 			break
 		except sr.UnknownValueError:
-			os.system('espeak "' + "Could you please repeat that" + '"')
+			os.system('./speech.sh ' + "Could you please repeat that")
 			print("error")
 
 
 		except sr.RequestError as e:
 			print"error"
-			os.system('espeak "' + "Could you please repeat that" + '"')
+			os.system('./speech.sh ' + "Could you please repeat that")
 
 	place = r.recognize_google(audio)
 	observation = owm.weather_at_place(place)
@@ -32,7 +33,7 @@ def weather():
 	complete_temp = w.get_temperature('celsius') 
 	for i in complete_temp:
 		if(i=="temp"):
-			os.system('espeak "' + "The temperature is " + str(complete_temp[i]) + " Degrees celsius " + '"')
+			os.system('./speech.sh ' + "The temperature is " + str(complete_temp[i]) + " Degrees celsius ")
 
 def news_for_today():
 	response = requests.get('https://news.google.com/news/section?cf=all&pz=1&topic=b&siidp=b458d5455b7379bd8193a061024cd11baa97&ict=ln')
@@ -41,8 +42,23 @@ def news_for_today():
 		news = pagehtml.xpath('//h2[@class="esc-lead-article-title"] \
 		                      /a/span[@class="titletext"]/text()')
 		for i in news:
-			os.system('espeak -s 140 -p 75 -a 200 "' + i + '"')
-			time.sleep(1)
+			os.system('./speech.sh ' + i)
 		#print("\n \n".join(news))
-		
-news_for_today()
+
+
+
+def current_time():
+	d=str(datetime.now().time())
+	d=d[:5]
+	d=datetime.strptime(d, "%H:%M")
+	d=d.strftime("%I:%M %p")
+	os.system('./speech.sh ' + d)
+
+def reminder():
+	
+
+#news_for_today()
+#current_time()
+
+
+
