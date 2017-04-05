4364b57ce2c2e44920dcb8d4de658d8cbd79bec8
YashitM <yashitmaheshwary@gmail.com>
2017-04-05 21:25:30 +0530

    removed indentation error

--- Locomotion/Move.py
+++ Locomotion/Move.py
@@ -3,38 +3,38 @@ import curses
 import RPi.GPIO as GPIO
 
 def findDistance():
-    GPIO.setmode(GPIO.BCM)
+	GPIO.setmode(GPIO.BCM)
 
-    TRIG = 23
-    ECHO = 24
-    print "[+] Distance Measurement in Progress"
+	TRIG = 23
+	ECHO = 24
+	print "[+] Distance Measurement in Progress"
 
-    GPIO.setup(TRIG, GPIO.OUT)
-    GPIO.setup(ECHO, GPIO.IN)
+	GPIO.setup(TRIG, GPIO.OUT)
+	GPIO.setup(ECHO, GPIO.IN)
 
-    GPIO.output(TRIG, False)
-    print "[+] Waiting for sensor to settle"
-    time.sleep(2)
+	GPIO.output(TRIG, False)
+	print "[+] Waiting for sensor to settle"
+	time.sleep(2)
 
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
 
-    while GPIO.input(ECHO)==0:
-    pulse_start = time.time()
+	while GPIO.input(ECHO)==0:
+	pulse_start = time.time()
 
-    while GPIO.input(ECHO)==1:
-    pulse_end = time.time()
+	while GPIO.input(ECHO)==1:
+	pulse_end = time.time()
 
-    pulse_duration = pulse_end - pulse_start
+	pulse_duration = pulse_end - pulse_start
 
-    distance = pulse_duration * 17150
+	distance = pulse_duration * 17150
 
-    ditance = round(distance, 2)
+	ditance = round(distance, 2)
 
-    print "Distance:", distance, "cm" # The final distance
-    GPIO.cleanup()
-    return distance
+	print "Distance:", distance, "cm" # The final distance
+	GPIO.cleanup()
+	return distance
 
 #set GPIO numbering mode and define output pins
 GPIO.setmode(GPIO.BOARD)
@@ -51,48 +51,48 @@ curses.cbreak()
 screen.keypad(True)
 
 try:
-        while True:   
-            curDistance = findDistance()
-            char = screen.getch()
-            if char == ord('q'):
-                break
-            elif char == ord('s'):
-                GPIO.output(7,False)
-                GPIO.output(11,False)
-                GPIO.output(13,False)
-                GPIO.output(15,False)
-            elif curDistance > 30:
-                if char == curses.KEY_RIGHT:
-                    GPIO.output(7,False)
-                    GPIO.output(11,True)
-                    GPIO.output(13,False)
-                    GPIO.output(15,True)
-                elif char == curses.KEY_LEFT:
-                    GPIO.output(7,True)
-                    GPIO.output(11,False)
-                    GPIO.output(13,True)
-                    GPIO.output(15,False)
-                elif char == curses.KEY_UP:
-                    GPIO.output(7,True)
-                    GPIO.output(11,False)
-                    GPIO.output(13,False)
-                    GPIO.output(15,True)
-                elif char == curses.KEY_DOWN:
-                    GPIO.output(7,False)
-                    GPIO.output(11,True)
-                    GPIO.output(13,True)
-                    GPIO.output(15,False)
-            else:
-                GPIO.output(7,False)
-                GPIO.output(11,True)
-                GPIO.output(13,False)
-                GPIO.output(15,True)
-                
-             
+	while True:   
+		curDistance = findDistance()
+		char = screen.getch()
+		if char == ord('q'):
+			break
+		elif char == ord('s'):
+			GPIO.output(7,False)
+			GPIO.output(11,False)
+			GPIO.output(13,False)
+			GPIO.output(15,False)
+		elif curDistance > 30:
+			if char == curses.KEY_RIGHT:
+				GPIO.output(7,False)
+				GPIO.output(11,True)
+				GPIO.output(13,False)
+				GPIO.output(15,True)
+			elif char == curses.KEY_LEFT:
+				GPIO.output(7,True)
+				GPIO.output(11,False)
+				GPIO.output(13,True)
+				GPIO.output(15,False)
+			elif char == curses.KEY_UP:
+				GPIO.output(7,True)
+				GPIO.output(11,False)
+				GPIO.output(13,False)
+				GPIO.output(15,True)
+			elif char == curses.KEY_DOWN:
+				GPIO.output(7,False)
+				GPIO.output(11,True)
+				GPIO.output(13,True)
+				GPIO.output(15,False)
+		else:
+			GPIO.output(7,False)
+			GPIO.output(11,True)
+			GPIO.output(13,False)
+			GPIO.output(15,True)
+			
+		 
 finally:
-    #Close down curses properly, inc turn echo back on!
-    curses.nocbreak(); screen.keypad(0); curses.echo()
-    curses.endwin()
-    GPIO.cleanup()
-    
+	#Close down curses properly, inc turn echo back on!
+	curses.nocbreak(); screen.keypad(0); curses.echo()
+	curses.endwin()
+	GPIO.cleanup()
+	
 
