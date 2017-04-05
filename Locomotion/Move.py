9decfa2b8f89d8ffec1e8ec55ebdd2d1e47e5958
YashitM <yashitmaheshwary@gmail.com>
2017-04-05 20:29:38 +0530

    added ultrasonic support to move.py

--- Locomotion/Move.py
+++ Locomotion/Move.py
@@ -1,7 +1,40 @@
-
-#import curses and GPIO
+import time
 import curses
 import RPi.GPIO as GPIO
+
+def findDistance():
+    GPIO.setmode(GPIO.BCM)
+
+    TRIG = 23
+    ECHO = 24
+    print "[+] Distance Measurement in Progress"
+
+    GPIO.setup(TRIG, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+
+    GPIO.output(TRIG, False)
+    print "[+] Waiting for sensor to settle"
+    time.sleep(2)
+
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+
+    while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
+
+    while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = pulse_duration * 17150
+
+    ditance = round(distance, 2)
+
+    print "Distance:", distance, "cm" # The final distance
+    GPIO.cleanup()
+    return distance
 
 #set GPIO numbering mode and define output pins
 GPIO.setmode(GPIO.BOARD)
@@ -19,34 +52,42 @@ screen.keypad(True)
 
 try:
         while True:   
+            curDistance = findDistance()
             char = screen.getch()
             if char == ord('q'):
                 break
-            elif char == curses.KEY_RIGHT:
-                GPIO.output(7,False)
-                GPIO.output(11,True)
-                GPIO.output(13,False)
-                GPIO.output(15,True)
-            elif char == curses.KEY_LEFT:
-                GPIO.output(7,True)
-                GPIO.output(11,False)
-                GPIO.output(13,True)
-                GPIO.output(15,False)
-            elif char == curses.KEY_UP:
-                GPIO.output(7,True)
-                GPIO.output(11,False)
-                GPIO.output(13,False)
-                GPIO.output(15,True)
-            elif char == curses.KEY_DOWN:
-                GPIO.output(7,False)
-                GPIO.output(11,True)
-                GPIO.output(13,True)
-                GPIO.output(15,False)
             elif char == ord('s'):
                 GPIO.output(7,False)
                 GPIO.output(11,False)
                 GPIO.output(13,False)
                 GPIO.output(15,False)
+            elif curDistance > 30:
+                if char == curses.KEY_RIGHT:
+                    GPIO.output(7,False)
+                    GPIO.output(11,True)
+                    GPIO.output(13,False)
+                    GPIO.output(15,True)
+                elif char == curses.KEY_LEFT:
+                    GPIO.output(7,True)
+                    GPIO.output(11,False)
+                    GPIO.output(13,True)
+                    GPIO.output(15,False)
+                elif char == curses.KEY_UP:
+                    GPIO.output(7,True)
+                    GPIO.output(11,False)
+                    GPIO.output(13,False)
+                    GPIO.output(15,True)
+                elif char == curses.KEY_DOWN:
+                    GPIO.output(7,False)
+                    GPIO.output(11,True)
+                    GPIO.output(13,True)
+                    GPIO.output(15,False)
+            else:
+                GPIO.output(7,False)
+                GPIO.output(11,True)
+                GPIO.output(13,False)
+                GPIO.output(15,True)
+                
              
 finally:
     #Close down curses properly, inc turn echo back on!
