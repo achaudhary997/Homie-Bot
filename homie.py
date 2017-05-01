01c4cef6a30b4d76c6b6dcb19543a494ae96ee1d
YashitM <yashitmaheshwary@gmail.com>
2017-05-01 23:46:27 +0530

    Indented input fucntion

--- homie.py
+++ homie.py
@@ -205,48 +205,49 @@ def trackObject(imgname, matchthresh):
 	counter = 0
 	while (camera.isOpened()):
 		ret, imgCamColor = camera.read()
-		imgCamGray = cv2.cvtColor(imgCamColor, cv2.COLOR_BGR2GRAY)
-		kpCam = orb.detect(imgCamGray,None)
-		kpCam, desCam = orb.compute(imgCamGray, kpCam)
-		matches = bf.match(desCam,desTrain)
-		dist = [m.distance for m in matches]
-		try:
-			thres_dist = (sum(dist) / len(dist)) * 0.5
-		except ZeroDivisionError:
-			thres_dist = (sum(dist) *0.5)
-		matches = [m for m in matches if m.distance < thres_dist]   
+		if ret:
+			imgCamGray = cv2.cvtColor(imgCamColor, cv2.COLOR_BGR2GRAY)
+			kpCam = orb.detect(imgCamGray,None)
+			kpCam, desCam = orb.compute(imgCamGray, kpCam)
+			matches = bf.match(desCam,desTrain)
+			dist = [m.distance for m in matches]
+			try:
+				thres_dist = (sum(dist) / len(dist)) * 0.5
+			except ZeroDivisionError:
+				thres_dist = (sum(dist) *0.5)
+			matches = [m for m in matches if m.distance < thres_dist]   
 
-		if firsttime==True:
-			h1, w1 = imgCamColor.shape[:2]
-			h2, w2 = imgTrainColor.shape[:2]
-			nWidth = w1+w2
-			nHeight = max(h1, h2)
-			hdif = (h1-h2)/2
-			firsttime=False
-		result = np.zeros((nHeight, nWidth, 3), np.uint8)
-		# print np.size(result)
-		result[hdif:hdif+h2, :w2] = imgTrainColor
-		result[:h1, w2:w1+w2] = imgCamColor
+			if firsttime==True:
+				h1, w1 = imgCamColor.shape[:2]
+				h2, w2 = imgTrainColor.shape[:2]
+				nWidth = w1+w2
+				nHeight = max(h1, h2)
+				hdif = (h1-h2)/2
+				firsttime=False
+			result = np.zeros((nHeight, nWidth, 3), np.uint8)
+			# print np.size(result)
+			result[hdif:hdif+h2, :w2] = imgTrainColor
+			result[:h1, w2:w1+w2] = imgCamColor
 
-		for i in range(len(matches)):
-			pt_a=(int(kpTrain[matches[i].trainIdx].pt[0]), int(kpTrain[matches[i].trainIdx].pt[1]+hdif))
-			pt_b=(int(kpCam[matches[i].queryIdx].pt[0]+w2), int(kpCam[matches[i].queryIdx].pt[1]))
-			cv2.line(result, pt_a, pt_b, (255, 0, 0))
+			for i in range(len(matches)):
+				pt_a=(int(kpTrain[matches[i].trainIdx].pt[0]), int(kpTrain[matches[i].trainIdx].pt[1]+hdif))
+				pt_b=(int(kpCam[matches[i].queryIdx].pt[0]+w2), int(kpCam[matches[i].queryIdx].pt[1]))
+				cv2.line(result, pt_a, pt_b, (255, 0, 0))
 
-		#cv2.imshow('Camara', result)
-		
-		key = cv2.waitKey(20)
-		if key == ESC:
-			break
-		if len(matches) > matchthresh or counter > 60: # or wait 3 seconds
-			if counter > 60:
-				return False, listOfMatches
-			cv2.destroyAllWindows()
-			camera.release()
-			return True, listOfMatches
-		counter+=1
-		listOfMatches.append(len(matches))
-		#print len(matches)
+			#cv2.imshow('Camara', result)
+			
+			key = cv2.waitKey(20)
+			if key == ESC:
+				break
+			if len(matches) > matchthresh or counter > 60: # or wait 3 seconds
+				if counter > 60:
+					return False, listOfMatches
+				cv2.destroyAllWindows()
+				camera.release()
+				return True, listOfMatches
+			counter+=1
+			listOfMatches.append(len(matches))
+			#print len(matches)
 	cv2.destroyAllWindows()
 	camera.release()
 
